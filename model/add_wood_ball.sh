f0d30b06c0cc2fb1ee3d93ed05e5a74fb3bbeee5
TakumuTakanezawa <takanen20010829kota@gmail.com>
2021-10-29 22:15:34 +0900

    add model

--- model/add_wood_ball.sh
+++ model/add_wood_ball.sh
@@ -1,2 +1,6 @@
 rm -rf ~/.gazebo/models/wood_ball_7cm/
 cp -r ./wood_ball_7cm ~/.gazebo/models/
+rm -rf ~/.gazebo/models/wood_box/
+cp -r ./wood_box ~/.gazebo/models/
+rm -rf ~/.gazebo/models/wood_pedestal/
+cp -r ./wood_pedestal ~/.gazebo/models/
