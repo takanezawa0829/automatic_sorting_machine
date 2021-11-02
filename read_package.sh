28c5b3cf47c0e195cbf6d04f3fddd0bf58b4cf62
TakumuTakanezawa <takanen20010829kota@gmail.com>
2021-11-02 10:32:01 +0900

    fix read_package.sh

--- read_package.sh
+++ read_package.sh
@@ -1,10 +1,9 @@
-cd ../../
+cd scripts/
+echo "ファイルに実行権限を付与します。"
+chmod +x *
+cd ../../../
 echo "catkin_makeを実行します。"
 catkin_make
 cd src
 cd automatic_sorting_machine
-cd scripts/
-echo "ファイルに実行権限を付与します。"
-chmod +x *
-cd ..
 echo "完了しました。"
